d121a1a204d68969c488d708e04f0a2e2eb6d6e8
Ирина Скрипка <irinaskripka99@gmail.com>
2020-04-25 22:45:36 +0300

    chande structure folder

--- Timetable/dayspan-vuetify/src/components/index.js
+++ Timetable/dayspan-vuetify/src/components/index.js
@@ -1,49 +1,49 @@
 
 export { default as DsGestures } from './Gestures';
 
-export { default as DsDay } from './Day';
-export { default as DsDayRow } from './DayRow';
-export { default as DsDaysView } from './DaysView';
-export { default as DsDayTimes } from './DayTimes';
-export { default as DsDayPicker } from './DayPicker';
+export { default as DsDay } from './Day/Day';
+export { default as DsDayRow } from './Day/DayRow';
+export { default as DsDaysView } from './Day/DaysView';
+export { default as DsDayTimes } from './Day/DayTimes';
+export { default as DsDayPicker } from './Day/DayPicker';
 
-export { default as DsAgenda } from './Agenda';
-export { default as DsAgendaDay } from './AgendaDay';
-export { default as DsAgendaEvent } from './AgendaEvent';
+export { default as DsAgenda } from './Agenda/Agenda';
+export { default as DsAgendaDay } from './Agenda/AgendaDay';
+export { default as DsAgendaEvent } from './Agenda/AgendaEvent';
 
-export { default as DsCalendar } from './Calendar';
-export { default as DsCalendarApp } from './CalendarApp';
-export { default as DsCalendarEvent } from './CalendarEvent';
-export { default as DsCalendarEventTime } from './CalendarEventTime';
-export { default as DsCalendarEventChip } from './CalendarEventChip';
-export { default as DsCalendarEventPopover } from './CalendarEventPopover';
-export { default as DsCalendarEventPlaceholder } from './CalendarEventPlaceholder';
-export { default as DsCalendarEventCreatePopover } from './CalendarEventCreatePopover';
-export { default as DsCalendarEventTimePlaceholder } from './CalendarEventTimePlaceholder';
+export { default as DsCalendar } from './Calendar/Calendar';
+export { default as DsCalendarApp } from './Calendar/CalendarApp';
+export { default as DsCalendarEvent } from './Calendar/CalendarEvent';
+export { default as DsCalendarEventTime } from './Calendar/CalendarEventTime';
+export { default as DsCalendarEventChip } from './Calendar/CalendarEventChip';
+export { default as DsCalendarEventPopover } from './Calendar/CalendarEventPopover';
+export { default as DsCalendarEventPlaceholder } from './Calendar/CalendarEventPlaceholder';
+export { default as DsCalendarEventCreatePopover } from './Calendar/CalendarEventCreatePopover';
+export { default as DsCalendarEventTimePlaceholder } from './Calendar/CalendarEventTimePlaceholder';
 
-export { default as DsEvent } from './Event';
-export { default as DsEventDialog } from './EventDialog';
+export { default as DsEvent } from './Events/Event';
+export { default as DsEventDialog } from './Events/EventDialog';
 
-export { default as DsSchedule } from './Schedule';
-export { default as DsScheduleSpan } from './ScheduleSpan';
-export { default as DsScheduleType } from './ScheduleType';
-export { default as DsScheduleTime } from './ScheduleTime';
-export { default as DsScheduleTimes } from './ScheduleTimes';
-export { default as DsScheduleActions } from './ScheduleActions';
-export { default as DsScheduleForecast } from './ScheduleForecast';
-export { default as DsScheduleModifier } from './ScheduleModifier';
-export { default as DsScheduleTypeCustom } from './ScheduleTypeCustom';
-export { default as DsScheduleTypeCustomDialog } from './ScheduleTypeCustomDialog';
+export { default as DsSchedule } from './Schedule/Schedule';
+export { default as DsScheduleSpan } from './Schedule/ScheduleSpan';
+export { default as DsScheduleType } from './Schedule/ScheduleType';
+export { default as DsScheduleTime } from './Schedule/ScheduleTime';
+export { default as DsScheduleTimes } from './Schedule/ScheduleTimes';
+export { default as DsScheduleActions } from './Schedule/ScheduleActions';
+export { default as DsScheduleForecast } from './Schedule/ScheduleForecast';
+export { default as DsScheduleModifier } from './Schedule/ScheduleModifier';
+export { default as DsScheduleTypeCustom } from './Schedule/ScheduleTypeCustom';
+export { default as DsScheduleTypeCustomDialog } from './Schedule/ScheduleTypeCustomDialog';
 
-export { default as DsScheduleFrequency } from './ScheduleFrequency';
-export { default as DsScheduleFrequencyDay } from './ScheduleFrequencyDay';
-export { default as DsScheduleFrequencyWeek } from './ScheduleFrequencyWeek';
-export { default as DsScheduleFrequencyYear } from './ScheduleFrequencyYear';
-export { default as DsScheduleFrequencyMonth } from './ScheduleFrequencyMonth';
-export { default as DsScheduleFrequencyDayOfWeek } from './ScheduleFrequencyDayOfWeek';
+export { default as DsScheduleFrequency } from './Schedule/ScheduleFrequency';
+export { default as DsScheduleFrequencyDay } from './Schedule/ScheduleFrequencyDay';
+export { default as DsScheduleFrequencyWeek } from './Schedule/ScheduleFrequencyWeek';
+export { default as DsScheduleFrequencyYear } from './Schedule/ScheduleFrequencyYear';
+export { default as DsScheduleFrequencyMonth } from './Schedule/ScheduleFrequencyMonth';
+export { default as DsScheduleFrequencyDayOfWeek } from './Schedule/ScheduleFrequencyDayOfWeek';
 
-export { default as DsWeeksView } from './WeeksView';
-export { default as DsWeekHeader } from './WeekHeader';
-export { default as DsWeekDayHeader } from './WeekDayHeader';
+export { default as DsWeeksView } from './Week/WeeksView';
+export { default as DsWeekHeader } from './Week/WeekHeader';
+export { default as DsWeekDayHeader } from './Week/WeekDayHeader';
 
 export { default as DsIdentifierChip } from './IdentifierChip';
